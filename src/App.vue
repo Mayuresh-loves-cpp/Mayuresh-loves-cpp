07040e13d13fd76af9716af786d7178ed3b39fab
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2024-01-01 23:29:43 +0530

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <button @click="go">Click Here to Animate</button>
-    <div class="block" ref="square"></div>
+    <button @click="expand ? back() : go()">Click Here to Animate</button>
+    <div class="block" ref="square">
+      <ul style=" color: aliceblue;">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -9,11 +15,35 @@
 import { ref } from "vue";
 import anime from "animejs";
 
-const square = ref();
+const square = ref(null);
+const expand = ref(true);
 
 function go() {
   console.log("exceuted");
-  anime({ targets: ".block", translateX: 500 });
+  anime({
+    targets: square.value,
+    width: "128px",
+    easing: 'easeInOutExpo'
+    // translateX: '500px',
+    // loop: true,
+    // easing: "easeInOutQuad",
+    // direction: "alternate",
+  });
+  expand.value = true;
+}
+
+function back() {
+  console.log("exceuted");
+  anime({
+    targets: square.value,
+    width: "500px",
+    easing: 'easeInOutExpo'
+    // translateX: '500px',
+    // loop: true,
+    // easing: "easeInOutQuad",
+    // direction: "alternate",
+  });
+  expand.value = false;
 }
 console.log(Object.keys(square));
 </script>
@@ -23,7 +53,7 @@ console.log(Object.keys(square));
   pointer-events: none;
   position: relative;
   width: 128px;
-  height: 128px;
+  height: 800px;
   margin: 1px;
   background-color: currentColor;
   font-size: 12px;
