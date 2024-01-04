f60e6747b5454a4b85c2969ceec6a9699067b476
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2024-01-04 18:04:12 +0530

    adding tty to portfolio

--- src/App.vue
+++ src/App.vue
@@ -36,13 +36,88 @@ import { ref, watch, onMounted } from "vue";
 import CommandLine from "./components/CommandLine.vue";
 import anime from "animejs";
 
-const square = ref();
+const keyPressed = ref();
+const ttyStack = ref([]);
+const ttyLine = 0;
 
-function go() {
-  console.log("exceuted");
-  anime({ targets: ".block", translateX: 500 });
+onMounted(() => {
+  window.addEventListener("keydown", function (ev) {
+    keyPressed.value = ev.key;
+    updateCurrentCommand(ev.key);
+    console.log("keydown");
+  });
+});
+
+function updateCurrentCommand(key) {
+  console.log(currentCommand.value.slice(0, currentCommand.value.length - 1));
+  // while (keyHold) {
+  if (key === "Backspace") {
+    currentCommand.value = currentCommand.value.slice(
+      0,
+      currentCommand.value.length - 1
+    );
+  } else if (key == "Enter") {
+    // const newSpan = document.createElement('span');
+    // newSpan.innerHTML = "{{ cwd }}{{ currentCommand }}{{ cursor }}";
+    // newSpan.setAttribute("style", "font-size: 20px");
+    // document.getElementById("root").appendChild(newSpan);
+    ttyStack.value.push({
+      cwd: cwd.value,
+      currentCommand: currentCommand.value,
+      cursor: "",
+    });
+    currentCommand.value = "";
+  } else {
+    currentCommand.value = currentCommand.value + key;
+  }
+  // }
 }
-console.log(Object.keys(square));
+
+const cwd = ref("(home) > ");
+
+const currentCommand = ref("");
+let cursorFlag = true;
+const cursor = ref("");
+setInterval(() => {
+  if (cursorFlag) {
+    cursor.value = "_";
+    cursorFlag = false;
+  } else {
+    cursor.value = "";
+    cursorFlag = true;
+  }
+}, 500);
+
+// const square = ref(null);
+
+// const isMenuOpen = ref(false);
+
+// watch(isMenuOpen, (newValue) => {
+//   if (newValue) {
+//     expandMenu();
+//   } else {
+//     closeMenu();
+//   }
+// });
+
+// function expandMenu() {
+//   console.log("exceuted");
+//   anime({
+//     targets: square.value,
+//     width: { value: "100%", duration: 300 },
+//     // height: { value: 100, duration: 500 },
+//     easing: "easeOutExpo",
+//   });
+// }
+
+// function closeMenu() {
+//   anime({
+//     targets: square.value,
+//     width: { value: "128px", duration: 500 },
+//     // height: { value: 128, duration: 300 },
+//     easing: "easeOutExpo",
+//   });
+// }
 </script>
 
 <style >
@@ -62,7 +137,7 @@ body {
 .block {
   /* position: relative; */
   width: 128px;
-  height: 800px;
+  height: 128px;
   margin: 1px;
   background-color: currentColor;
   font-size: 12px;
